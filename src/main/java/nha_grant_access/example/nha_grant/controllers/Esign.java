e8e9c95104aa64715338ee20a1c698eabefe5277
VaradPatel <varad.rupeshpatel.cd.civ19@itbhu.ac.in>
2025-06-16 12:44:03 +0530

    esign status

--- src/main/java/nha_grant_access/example/nha_grant/controllers/Esign.java
+++ src/main/java/nha_grant_access/example/nha_grant/controllers/Esign.java
@@ -2,10 +2,7 @@ package nha_grant_access.example.nha_grant.controllers;
 
 import lombok.extern.slf4j.Slf4j;
 import nha_grant_access.example.nha_grant.dto.Error;
-import nha_grant_access.example.nha_grant.dto.Esign.Document;
-import nha_grant_access.example.nha_grant.dto.Esign.DocumentRequest;
-import nha_grant_access.example.nha_grant.dto.Esign.EspResponse;
-import nha_grant_access.example.nha_grant.dto.Esign.MatchAadharDetailsTO;
+import nha_grant_access.example.nha_grant.dto.Esign.*;
 import nha_grant_access.example.nha_grant.entity.GrantRequests;
 import nha_grant_access.example.nha_grant.entity.User;
 import nha_grant_access.example.nha_grant.repository.IGrantRequestsRepo;
@@ -15,6 +12,7 @@ import nha_grant_access.example.nha_grant.service.EsignService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -33,7 +31,8 @@ public class Esign {
     @Autowired
     IGrantRequestsRepo iGrantRequestsRepo;
     @PostMapping("/generate-request/{userId}/{requestId}")
-    public ResponseEntity<?> generateESignRequest(@RequestBody DocumentRequest documentRequest, @PathVariable Integer userId ,@PathVariable String requestId) {
+    @PreAuthorize("hasAuthority('State CEO') ")
+    public ResponseEntity<?> generateESignRequest(@RequestBody DocumentRequest documentRequest, @PathVariable Integer userId ,@PathVariable String requestId){
         try {
             User user=userRepo.findById(userId).get();
             GrantRequests grantRequests=iGrantRequestsRepo.findGrantRequestByRequestId(requestId);
@@ -60,6 +59,25 @@ documentRequest.getDocument().setSigningPlace(documentRequest.getDocument().getS
                     .body(new Error("Error while calling DigiSign API: "  ,  e.getMessage() ));
         }
     }
+@GetMapping("/status/{requestId}")
+@PreAuthorize("hasAuthority('State CEO') ")
+    public ResponseEntity<?> CheckEsignStatus(@PathVariable("requestId") String requestId)
+{
+    try {
+        GrantRequests grantRequests=iGrantRequestsRepo.findByRequestId(requestId).get();
+        StatusResponse statusResponse=new StatusResponse();
+        statusResponse.setEsign_status(grantRequests.getESignStatusStateCeo());
+        statusResponse.setEsign_certificate(grantRequests.getEsign_pdf());
+       return ResponseEntity.ok().body(statusResponse);
+
+    }
+    catch (Exception e)
+    {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new Error("Error while checking ESign Status: "  ,  e.getMessage() ));
+
+    }
+}
 
 
 
---
+++ src/main/java/nha_grant_access/example/nha_grant/dto/Esign/StatusResponse.java
@@ -0,0 +1,16 @@
+package nha_grant_access.example.nha_grant.dto.Esign;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class StatusResponse {
+
+    private boolean esign_status;
+    private byte[] esign_certificate;
+}
