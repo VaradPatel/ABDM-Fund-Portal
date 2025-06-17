127e554c4dd0e53de1e447106fe5b41e863827cb
VaradPatel <varad.rupeshpatel.cd.civ19@itbhu.ac.in>
2025-06-17 15:35:54 +0530

    esign

--- src/main/java/nha_grant_access/example/nha_grant/repository/IUserStateRoleRepo.java
+++ src/main/java/nha_grant_access/example/nha_grant/repository/IUserStateRoleRepo.java
@@ -10,7 +10,7 @@ import java.util.List;
 public interface IUserStateRoleRepo extends JpaRepository<UserStateRole, Integer> {
     @Query("SELECT usr FROM UserStateRole usr WHERE usr.user.id = :userId")
     List<UserStateRole> getUserStateRoleByUserid(Integer userId);
-    @Query(value = "SELECT * FROM user_state_role WHERE role_id = :roleId AND state_id IN (:stateIds)", nativeQuery = true)
+    @Query(value = "SELECT * FROM user_state_role WHERE role_id = :roleId AND state_id IN (:stateId)", nativeQuery = true)
 
     List<UserStateRole>getUserByStateAndRole(Integer roleId, List<Integer>stateId );
 
