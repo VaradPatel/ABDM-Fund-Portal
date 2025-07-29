5b2d579566af0878a07aaa1289236e52425e86a6
VaradPatel <varad.rupeshpatel.cd.civ19@itbhu.ac.in>
2025-07-29 15:10:50 +0530

    prod testing

--- src/main/java/nha_grant_access/example/nha_grant/repository/IGrantRequestsRepo.java
+++ src/main/java/nha_grant_access/example/nha_grant/repository/IGrantRequestsRepo.java
@@ -71,7 +71,7 @@ GrantRequests findGrantRequestByRequestId(String requestId);
             "WHERE gr.state_id = :stateId " +
             "AND q.active = true " +
             "AND ( " +
-            "     (u.role_id = 3 AND gr.flow_status = 11) " +
+            "     (u.role_id = 3 AND gr.flow_status = 11 and gr.query_to_role_id=2) " +
             "  OR (u.role_id = 4 AND gr.flow_status = 10) " +
             ")",
             nativeQuery = true)
@@ -83,7 +83,7 @@ GrantRequests findGrantRequestByRequestId(String requestId);
             "JOIN queries q ON q.request_id = gr.request_id " +
             "JOIN users u ON q.query_user_id = u.id " +
             "JOIN states s ON gr.state_id = s.id " +
-            "WHERE gr.state_id IN (:stateId) AND gr.flow_status = 7 AND q.active = true and u.role_id = 4 and q.query_to_role_id=2  ",
+            "WHERE gr.state_id IN (:stateId) AND gr.flow_status = 7 AND q.active = true and u.role_id = 4 ",
             nativeQuery = true)
     List<Object[]> findStateCordActiveQuery(List<Integer>stateId);
     boolean existsByRequestId(String requestId);
