1becdaed9be546df22a61d7bf3e6fb5cb104c2e0
VaradPatel <varad.rupeshpatel.cd.civ19@itbhu.ac.in>
2025-08-11 13:25:09 +0530

    cors removed

--- src/main/java/nha_grant_access/example/nha_grant/config/CorsConfig.java
+++ src/main/java/nha_grant_access/example/nha_grant/config/CorsConfig.java
@@ -37,13 +37,13 @@ public class CorsConfig {
 //            }
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-//                        .allowedOrigins("*")
+                      .allowedOrigins("*")
 //                        .allowedOrigins(
 //                                "https://fundreleasesbx.nha.gov.in",
 //                                "http://fundreleasesbx.nha.gov.in",
 //                                "http://10.20.4.110:3000"
 //                        )
-                        .allowedOrigins(corsurl,"http://10.20.4.110:3000","http://loclahost:3000")
+                      //  .allowedOrigins(corsurl,"http://10.20.4.110:3000","http://loclahost:3000")
                         .allowedMethods("*")
                         //               .allowCredentials(true)
                         .exposedHeaders(ACCESS_TOKEN, REFRESH_TOKEN, CORRELATION_ID, CHECKSUM_HEADER);
